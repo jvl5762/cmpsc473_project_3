90b142a92673b50e484a012e140b3d663b0bd6d7
jvl5762 <jvl5762@psu.edu>
2017-04-26 14:51:22 -0400

    Create memoryManager.h

---
+++ memoryManager.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File           : memoryManager.h
+//  Description    : extension of virtualMemoryManager abstract base class
+//
+//  Author         : Jeff Li, Frank Guo, Chen Dikan
+//  Last Modified  : 04/28/2017
+#pragma once
+
+#include "virtualMemoryManagerInterface.hpp"
+
+// data structure for memory map
+typedef struct {
+   unsigned int page;
+   int time;
+} MemoryMap;
+
+class memoryManager: public virtualMemoryManagerInterface {
+public:
+	memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA) : virtualMemoryManagerInterface(p, pS, nF, vA) {}
+	unsigned long long memoryAccess(unsigned long long address) override; //Function to implement memory mapping policy
+
+	void initializeMemoryMap(unsigned int addressLimit);
+};
